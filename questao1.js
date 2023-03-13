283951836ea39b07e6d6b7dcac844ab2a9a98470
dedojs <xandresousax@gmail.com>
2023-03-13 17:49:29 -0300

    feat: questao 2

--- questao1.js
+++ questao1.js
@@ -1,3 +1,21 @@
+// 1) Observe o trecho de código abaixo:
+
+// int INDICE = 13, SOMA = 0, K = 0;
+
+// enquanto K < INDICE faça
+
+// {
+
+// K = K + 1;
+
+// SOMA = SOMA + K;
+
+// }
+
+// imprimir(SOMA);
+
+// Ao final do processamento, qual será o valor da variável SOMA?
+
 let indice = 13;
 let soma = 0;
 let k = 0;
@@ -8,3 +26,4 @@ for (var i = 0; k < indice; i ++) {
 };
 
 console.log(soma);
+// resultado: 91
